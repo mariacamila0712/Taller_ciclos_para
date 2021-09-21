376523723caf6664e96104178c8d9411706c3d50
MariaCamila <mariacmaza7@gmail.com>
2021-09-20 23:38:43 -0500

    Ejercicio 7 desarrollado

--- untitled8.py
+++ untitled8.py
@@ -5,12 +5,28 @@ Created on Mon Sep 20 23:22:47 2021
 @author: Maria Camila
 """
 
-peso = 0
-for i in range(1, 6):
-    peso_anterior = int((input("Digite el peso anterior del miembro n°" + str(i)+": ")))
-    for j in range(1, 11):
-     peso += int((input("Digite el peso de la báscula n°"+ str(j)+ " del miembro " + str(i)+": ")))     
-    if(peso_anterior-peso < 0):
-     print("El miembro bajó: ", peso_anterior - (peso/10), " kilos.")
-    if(peso_anterior-peso > 0):
-     print("El miembro subió: ", peso_anterior - (peso/10), " kilos.")
+# En un supermercado una ama de casa pone en su carrito los artículos que
+# va tomando de los estantes. La señora quiere asegurarse de que el cajero
+# le cobre bien lo que ella ha comprado, por lo que cada vez que toma un
+# artóculo anota su precio junto con la cantidad de artículos iguales que ha
+# tomado y determina cuanto dinero gastará en ese artículo; a esto le suma lo
+# que irá gastando en los demás artículos, hasta que decide que ya tomó
+# todo lo que necesitaba. Ayúdele a esta señora a obtener el total de su
+# compra.
+def compra_total():
+  articulos = []
+  total_compra = 0
+
+
+  for n in range( 1, 6):
+    precio = float(input(f'Digite el precio del articulo {n} $: '))
+    articulos.append(precio)
+
+  for articulo in articulos:
+    cantidad = int(input(f'Digite la cantidad del articulo {articulo}: '))
+    total = articulo * cantidad
+    total_compra += total 
+  
+  print(compra_total)
+
+compra_total()
